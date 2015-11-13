8eb1c70f5075d2b8d710843986e4868556137edf
Yusuke Sabi <y-sabi@ist.osaka-u.ac.jp>
2015-11-13 15:54:49 +0900

    TargetItemのstatic化

--- src/posmining/target/TargetItem.java
+++ src/posmining/target/TargetItem.java
@@ -4,7 +4,7 @@ public class TargetItem {
 
 	private static final String[] TARGET_CATEGORYS = {"滋養強壮ドリンク（一般品）","滋養強壮ドリンク（医薬部外品）","栄養補給ドリンク","ビネガードリンク","栄養補給錠剤・錠菓","栄養補給菓子","その他栄養補給食品"};
 
-	public boolean isTargetItem(String category){
+	public static boolean isTargetItem(String category){
 		for(String targetCategory : TARGET_CATEGORYS){
 			if(targetCategory.equals(category)){
 				return true;
